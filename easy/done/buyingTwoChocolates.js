b91449458108c1e25e6be43525f0001c24ab226e
ElrohiFilmon <84759101+ElrohiFilmon@users.noreply.github.com>
2024-03-17 20:34:46 +0300

    Update buyingTwoChocolates.js

--- easy/done/buyingTwoChocolates.js
+++ easy/done/buyingTwoChocolates.js
@@ -3,34 +3,34 @@
 //  * @param {number} val
 //  * @return {number}
 //  */
-// var removeElement = function(nums, val,expecteedNums = [],start = 0,end=nums.length -1,count=0) {
+var removeElement = function(nums, val,expecteedNums = [],start = 0,end=nums.length -1,count=0) {
 
-//     nums.sort((a,b)=>{return a - b});
-//     console.log(nums);
+    nums.sort((a,b)=>{return a - b});
+    console.log(nums);
     
-//     while(start<end){
-//         if(nums[start]==val && nums[end]!=val){
-//             nums[start] = nums[end]
-//             nums[end] = '_'
+    while(start<end){
+        if(nums[start]==val && nums[end]!=val){
+            nums[start] = nums[end]
+            nums[end] = '_'
 
-//         }
-//         end--
-//         start++
-//     }
-//     // nums.map(el => el === '_'? nums.splice(nums.indexOf(el), 1): el);
+        }
+        end--
+        start++
+    }
+    // nums.map(el => el === '_'? nums.splice(nums.indexOf(el), 1): el);
 
-//     for(var i = nums.length - 1; i >= 0; i--){
-//         if(nums[i] === '_') nums.pop();
-//     }
-//     // while(start<ftl){
-//     //     if(nums[ftl]=='_'){
-//     //         nums.pop();
-//     //         ftl--;
-//     //     }
-//     // }
+    for(var i = nums.length - 1; i >= 0; i--){
+        if(nums[i] === '_') nums.pop();
+    }
+    // while(start<ftl){
+    //     if(nums[ftl]=='_'){
+    //         nums.pop();
+    //         ftl--;
+    //     }
+    // }
 
-//     // nums.sort((a,b)=>{return b - a});
+    // nums.sort((a,b)=>{return b - a});
 
-// console.log(nums)
-// };
+console.log(nums)
+};
 
