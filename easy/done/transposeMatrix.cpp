36b6a996b21cf77bb0b29eb26f8a1bb732800ecd
ElrohiFilmon <elrohifilmon@gmail.com>
2024-03-23 21:24:24 +0300

    Just submitted the transpose matrix question already solved it wednesday when i was studying for my programming exam on modular programming through recursive functions, But i had to come back and look at vectors then after going through them tada

---
+++ easy/done/transposeMatrix.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int rows = matrix.size(); 
+        int cols = matrix[0].size(); 
+
+        vector<vector<int>> transposed(cols, vector<int>(rows)); 
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                transposed[j][i] = matrix[i][j]; 
+            }
+        }
+
+        return transposed;
+    }
+};
